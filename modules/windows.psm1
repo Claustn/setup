caa5e0cef4e4b0142dc20fbbaae7820df876bcc8
Claus T Nielsen <claustn@gmail.com>
2024-04-27 18:59:23 +0200

    Update windows.psm1

--- modules/windows.psm1
+++ modules/windows.psm1
@@ -12,36 +12,36 @@ function Uninstall-WindowsFeature($feature) {
     }
 }
 
-function Install-Ubuntu() {
-    Install-WindowsFeature Microsoft-Windows-Subsystem-Linux
-    $ProgressPreference = 'SilentlyContinue'
-    Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
-    Add-AppxPackage -Path .\Ubuntu.appx
-    Remove-Item -Path .\Ubuntu.appx -Force
-}
+#function Install-Ubuntu() {
+#    Install-WindowsFeature Microsoft-Windows-Subsystem-Linux
+#    $ProgressPreference = 'SilentlyContinue'
+#    Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
+#    Add-AppxPackage -Path .\Ubuntu.appx
+#    Remove-Item -Path .\Ubuntu.appx -Force
+#}
 
-function Uninstall-Ubuntu() {
-    Uninstall-WindowsFeature Microsoft-Windows-Subsystem-Linux
-    Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu18.04onWindows | Remove-AppxPackage
-}
+#function Uninstall-Ubuntu() {
+#    Uninstall-WindowsFeature Microsoft-Windows-Subsystem-Linux
+#    Get-AppxPackage -Name CanonicalGroupLimited.Ubuntu18.04onWindows | Remove-AppxPackage
+#}
 
-function Install-StartLayout([string]$fileName) {
-    # Unfortunately the function above imports the layout only for the "Default user"
-    Import-StartLayout -LayoutPath $fileName -MountPath $env:SystemDrive\
-    
-    # so we have to import it to the user manually.
-    $layoutDestinationPath = Join-Path $env:LOCALAPPDATA "Microsoft/Windows/Shell"
-    $dstFile = Join-Path $layoutDestinationPath "LayoutModification.xml"
-
-    New-MakeDirectoryForce $layoutDestinationPath
-    Copy-Item $fileName $dstFile -Force
-
-    # This will reset the start menu
-    reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" /va /f
-
-    # Restart explorer to apply the changes
-    Stop-Process -ProcessName "explorer"
-}
+#function Install-StartLayout([string]$fileName) {
+#    # Unfortunately the function above imports the layout only for the "Default user"
+#    Import-StartLayout -LayoutPath $fileName -MountPath $env:SystemDrive\
+#    
+#    # so we have to import it to the user manually.
+#    $layoutDestinationPath = Join-Path $env:LOCALAPPDATA "Microsoft/Windows/Shell"
+#    $dstFile = Join-Path $layoutDestinationPath "LayoutModification.xml"
+#
+#    New-MakeDirectoryForce $layoutDestinationPath
+#    Copy-Item $fileName $dstFile -Force
+#
+#    # This will reset the start menu
+#    reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount" /va /f
+#
+#    # Restart explorer to apply the changes
+#    Stop-Process -ProcessName "explorer"
+#}
 
 function Set-RegistryValue([string]$path, [string]$key, $value) {
     reg add $path /t REG_DWORD /f /v $key /d $value
@@ -120,23 +120,23 @@ function Disable-Telemetry() {
     Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DataCollection" "AllowTelemetry" "0"
 }
 
-function Set-DisableWindowsDefender([bool]$enable) {
-    # Disables Windows Defender. Also impacts third-party antivirus software and apps.
-    # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/security-malware-windows-defender-disableantispyware
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" "DisableAntiSpyware" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" "DisableRoutinelyTakingAction" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableBehaviorMonitoring" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableOnAccessProtection" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableScanOnRealtimeEnable" "1"
-}
+#function Set-DisableWindowsDefender([bool]$enable) {
+#    # Disables Windows Defender. Also impacts third-party antivirus software and apps.
+#    # https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/security-malware-windows-defender-disableantispyware
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" "DisableAntiSpyware" "1"
+#   Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender" "DisableRoutinelyTakingAction" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableBehaviorMonitoring" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableOnAccessProtection" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" "DisableScanOnRealtimeEnable" "1"
+#}
 
-function Set-DarkTheme([bool]$enable) {
-    Set-RegistryBool "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" "AppsUseLightTheme" ($enable -eq -$false)
-}
+#function Set-DarkTheme([bool]$enable) {
+#    Set-RegistryBool "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" "AppsUseLightTheme" ($enable -eq -$false)
+#}
 
-function Set-ColorTheme([string]$color) {
-    Set-RegistryBool "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" "AppsUseLightTheme" !$enable
-}
+#function Set-ColorTheme([string]$color) {
+#    Set-RegistryBool "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" "AppsUseLightTheme" !$enable
+#}
 
 function Disable-AdministratorSecurityPrompt() {
     # This option SHOULD be used to disable the automatic detection of installation packages that require elevation to install.
@@ -145,7 +145,7 @@ function Disable-AdministratorSecurityPrompt() {
 
     # Disabling this policy disables secure desktop prompting. All credential or consent prompting will occur on the interactive user's desktop.
     # https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-gpsb/9ad50fd3-4d8d-4870-9f5b-978ce292b9d8
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" "PromptOnSecureDesktop" "0"
+   # Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" "PromptOnSecureDesktop" "0"
 
     # This option allows the Consent Admin to perform an operation that requires elevation without consent or credentials.
     # https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-gpsb/341747f5-6b5d-4d30-85fc-fa1cc04038d4
@@ -173,31 +173,31 @@ function Remove-3dObjectsFolder {
     Remove-RegistryFolder "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}"
 }
 
-function Disable-IntelPowerThrottling {
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyDeviceIDs" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyDeviceIDsRetroactive" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyInstanceIDs" "1"
-    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyInstanceIDsRetroactive" "0"
-    
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "1" "*INT3400"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "2" "*INT3402"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "3" "*INT3403"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "4" "*INT3404"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "5" "*INT3407"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "6" "*INT3409"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "7" "PCI\VEN_8086&DEV_1603&CC_1180"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "8" "PCI\VEN_8086&DEV_1903&CC_1180"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "9" "PCI\VEN_8086&DEV_8A03&CC_1180"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "10" "PCI\VEN_8086&DEV_9C24&CC_1180"
-    
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "1" "ACPI\VEN_INT&DEV_3403"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "2" "ACPI\INT3403"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "3" "*INT3403"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "4" "ACPI\VEN_INT&DEV_3400"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "5" "ACPI\INT3400"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "6" "*INT3400"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "7" "PCI\VEN_8086&DEV_1903&SUBSYS_07BE1028&REV_05"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "8" "PCI\VEN_8086&DEV_1903&SUBSYS_07BE1028"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "9" "PCI\VEN_8086&DEV_1903&CC_118000"
-    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "10" "PCI\VEN_8086&DEV_1903&CC_1180"
-}
+#function Disable-IntelPowerThrottling {
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyDeviceIDs" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyDeviceIDsRetroactive" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyInstanceIDs" "1"
+#    Set-RegistryValue "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" "DenyInstanceIDsRetroactive" "0"
+#    
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "1" "*INT3400"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "2" "*INT3402"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "3" "*INT3403"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "4" "*INT3404"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "5" "*INT3407"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "6" "*INT3409"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "7" "PCI\VEN_8086&DEV_1603&CC_1180"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "8" "PCI\VEN_8086&DEV_1903&CC_1180"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "9" "PCI\VEN_8086&DEV_8A03&CC_1180"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" "10" "PCI\VEN_8086&DEV_9C24&CC_1180"
+#    
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "1" "ACPI\VEN_INT&DEV_3403"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "2" "ACPI\INT3403"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "3" "*INT3403"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "4" "ACPI\VEN_INT&DEV_3400"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "5" "ACPI\INT3400"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "6" "*INT3400"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "7" "PCI\VEN_8086&DEV_1903&SUBSYS_07BE1028&REV_05"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "8" "PCI\VEN_8086&DEV_1903&SUBSYS_07BE1028"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "9" "PCI\VEN_8086&DEV_1903&CC_118000"
+#    Set-RegistryString "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyInstanceIDs" "10" "PCI\VEN_8086&DEV_1903&CC_1180"
+#}
